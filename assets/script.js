915ffa47a5e3cc1fd1a22559799000a586c94da6
jpcreativeworks <jp.graphics.011@gmail.com>
2022-03-25 18:01:00 -0700

    found starter code for calendar

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+// this repeating the calendar rows
+
+let calendarRows = $("#cal-rows") 
+
+// console.log("calendarRows", calendarRows)
+
+$("#currentDay").html(moment().format('MMMM Do YYYY, h:mm:ss a'));
