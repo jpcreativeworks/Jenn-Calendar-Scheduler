c8eefe30d9e52c0cf92163e9a286fa0745b47ed0
jpcreativeworks <jp.graphics.011@gmail.com>
2022-03-25 18:21:18 -0700

    updated moment to generate every ten miliseconds

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,8 @@
 // this repeating the calendar rows
 
 let calendarRows = $("#cal-rows") 
-
+let secondsId = setInterval(() => $("#currentDay").html(moment().format('MMMM Do YYYY, h:mm:ss a')), 10)
 // console.log("calendarRows", calendarRows)
-
+//current time
 $("#currentDay").html(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
